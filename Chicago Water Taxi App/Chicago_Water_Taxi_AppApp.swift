32ac5f82e46ae0f7f6424a8560ac4512ae4a2dc9
ME <ME>
2026-07-06 10:56:39 -0500

    Initial Commit

---
+++ Chicago Water Taxi App/Chicago_Water_Taxi_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chicago_Water_Taxi_AppApp.swift
+//  Chicago Water Taxi App
+//
+//  Created by DPI Student 025 on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Chicago_Water_Taxi_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chicago Water Taxi App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chicago Water Taxi App
+//
+//  Created by DPI Student 025 on 7/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
